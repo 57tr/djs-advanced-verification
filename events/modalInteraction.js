3ed7f04e13562f651cd6469341f889d38da24e10
57tr <96021196+57tr@users.noreply.github.com>
2024-02-26 15:41:35 -0500

    Update verification system v2.0.

--- events/modalInteraction.js
+++ events/modalInteraction.js
@@ -1,60 +1,70 @@
 const {
     Client,
-    Interaction,
+    ChatInputCommandInteraction,
     EmbedBuilder
-} = require("discord.js");
+} = require('discord.js');
 
-const verifySchema = require("../schemas/verifySchema");
-const codesSchema = require("../schemas/codesSchema");
+const verifySchema = require('../../schemas/verifySchema');
+const codesSchema = require('../../schemas/codesSchema');
 
 module.exports = {
-    name: "interactionCreate",
+    name: 'interactionCreate',
+    /**
+     * ! Check the order of how you pass parameters in your slashcommand handler
+     */
 
     /**
      * @param {Client} client 
-     * @param {Interaction} interaction 
+     * @param {ChatInputCommandInteraction} interaction 
      * @returns 
      */
     async execute(client, interaction) {
 
         if (!interaction.isModalSubmit()) return;
-        
+
         if (!interaction.guild || !interaction.channel || !interaction.user || interaction.user.bot) return;
 
         const { fields, guild, member, customId } = interaction;
 
-        const dataVerify = await verifySchema.findOne({ guildId: guild.id });
-        const dataCode = await codesSchema.findOne({ userId: member.id });
+        switch (customId) {
+            case 'captcha-modal': {
+                const valid = fields.getTextInputValue('captcha-code');
 
-        if (customId === "captcha-modal") {
-            const code = fields.getTextInputValue("captcha-code");
+                const dataVerify = await verifySchema.findOne({ guildId: guild.id });
+                const dataCode = await codesSchema.findOne({ userId: member.id });
 
-            if (!dataCode && dataCode.userId !== member.id) return interaction.reply({
-                content: "We are having trouble verifying you, please try again.",
-            });
-
-            if (dataCode.captchaCode !== code) {
-                return interaction.reply({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setDescription(`Invalid captcha code: ${code}`)
-                            .setColor("Red")
-                    ],
-                    ephemeral: true,
+                if (!dataCode && dataCode.userId !== interaction.member.id) return interaction.reply({
+                    content: 'We are having trouble verifying you, please try again.',
                 });
-            } else {
-                await codesSchema.findOneAndUpdate({ userId: member.id },
-                    { captchaCode: `verified` },
-                    { new: true, upsert: true }
-                );
 
-                member.roles.add(dataVerify.roleId);
+                if (dataCode.captchaCode !== valid) {
+                    return interaction.reply({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setDescription(`Invalid captcha code: ${valid}`)
+                                .setColor('Red')
+                        ],
+                        ephemeral: true,
+                    });
+                } else {
+                    await codesSchema.findOneAndUpdate({ userId: member.id },
+                        { captchaCode: 'verified' },
+                        { new: true, upsert: true }
+                    );
 
-                return await interaction.reply({
-                    content: `<a:checkmark:1081679442595823686> We have verified that you are not a robot. Welcome to our server have a good time.`,
-                    ephemeral: true,
-                });
+                    interaction.member.roles.add(dataVerify.roleId);
+
+                    await interaction.reply({
+                        content: '✅ We have verified that you are not a robot. Welcome to our server have a good time.',
+                        ephemeral: true,
+                    });
+                }
+
+                break;
             }
+
+            default:
+                break;
         }
     }
 }
